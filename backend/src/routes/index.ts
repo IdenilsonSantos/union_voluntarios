4888e97e365ea65ceaaae1118e828e7a9e638801
Idenilson Santos <idenilsonoliveira.santos@gmail.com>
2024-08-11 16:17:02 -0300

    update: server routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,7 @@
-import { Router } from 'express';
+import express from 'express';
 import entitiesRoutes from './entities.routes';
 
-const router = Router();
+const router = express.Router();
 
 router.use('/entities', entitiesRoutes);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,3 +16,5 @@ app.use(routes);
 app.use(errorHandler);
 
 app.listen(3333, () => console.log('Server running on port 3333'));
+
+module.exports = app
